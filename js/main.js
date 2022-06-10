b66e3436336964fe801af3883d35071b8ebef329
Modar-Mulla <moddar8moulla@gmail.com>
2022-06-11 00:52:57 +0300

    Added Success Page

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 // Scroll To Top
 const scrollToTopBtn = document.querySelector(".back-to-home");
-window.addEventListener("scroll", function () {
-  if (scrollY > 200) {
-    scrollToTopBtn.classList.add("show-btn");
-  }
-});
-window.addEventListener("scroll", function () {
-  if (scrollY < 200) {
-    scrollToTopBtn.classList.remove("show-btn");
-  }
-});
-scrollToTopBtn.addEventListener("click", function () {
-  window.scrollTo({ top: 0 });
-});
+if (scrollToTopBtn !== null) {
+  window.addEventListener("scroll", function () {
+    if (scrollY > 200) {
+      scrollToTopBtn.classList.add("show-btn");
+    }
+  });
+  window.addEventListener("scroll", function () {
+    if (scrollY < 200) {
+      scrollToTopBtn.classList.remove("show-btn");
+    }
+  });
+  scrollToTopBtn.addEventListener("click", function () {
+    window.scrollTo({ top: 0 });
+  });
+}
 
 // Dark-Light Switch
 
 let mode = window.localStorage.getItem("mode");
 const switchBtn = document.querySelector(".dark-light");
-
-
 
 const switchToDark = () => {
   document.body.classList.add("dark-mode");
@@ -31,14 +31,15 @@ const switchToLight = () => {
 if (mode == "dark") {
   switchToDark();
 }
-
-switchBtn.addEventListener("click", function () {
-  mode = window.localStorage.getItem("mode");
-  if (mode == "dark") {
-    switchToLight();
-    window.localStorage.setItem("mode", "light");
-  } else {
-    switchToDark();
-    window.localStorage.setItem("mode", "dark");
-  }
-});
+if (switchBtn !== null) {
+  switchBtn.addEventListener("click", function () {
+    mode = window.localStorage.getItem("mode");
+    if (mode == "dark") {
+      switchToLight();
+      window.localStorage.setItem("mode", "light");
+    } else {
+      switchToDark();
+      window.localStorage.setItem("mode", "dark");
+    }
+  });
+}
